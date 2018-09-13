2f3943dc7ece13449943a4c452d8fa80d4cca155
lmmLucasMachado <lmlucasmachadom@gmail.com>
2018-09-13 00:22:36 -0300

    [ADD] fga-eps-mds/2018.2-GamesBI#83 Creating get to the id of the video

--- YoutubeService/importdata/models.py
+++ YoutubeService/importdata/models.py
@@ -2,21 +2,15 @@ from django.db import models
 
 
 class YouTubeSearch(models.Model):
-    
+
     id = models.CharField(
         ('Video ID'),
         help_text=("Video ID"),
         max_length=100,
         primary_key=True
     )
-    '''
-    name = models.CharField(
-        ('Name'),
-        help_text=('Name of the game'),
-        max_length=100,
-        null=True
-    )
 
+    '''
     count_views = models.IntegerField(
         ('count_views'),
         help_text=('Numbers of video views'),
@@ -35,19 +29,19 @@ class YouTubeSearch(models.Model):
         null=True
     )
 
+    count_favorites = models.IntegerField(
+        ('count_dislikes'),
+        help_text=("Number of dislikes"),
+        null=True
+    )
+
     count_comments = models.IntegerField(
         ('count_comment'),
         help_text=('Number of comment'),
         null=True
     )
     '''
-    regionCode = models.CharField(
-        ('regionCode'),
-        help_text=(''),
-        max_length=10,
-        null=True
-    )
-    
+
     def __str__(self):
         """
 	    Returns the object as a string, the attribute that will represent
--- YoutubeService/importdata/views.py
+++ YoutubeService/importdata/views.py
@@ -18,108 +18,111 @@ class YouTubeView(APIView):
         header = {'user-key': 'AIzaSyDmDXP_gaB7cog4f0slbbdJ3RACsY5WQIw',
         'Accept': 'application/json'}
         url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q={}&key={}'.format('PUBG', header['user-key'])
-        data = requests.get(url)#, headers=header)
+        data = requests.get(url)
         ndata = data.json()
 
-        
+
         filtered_data = self.filter_data(ndata)
         self.save_youtube_search(filtered_data)
         '''
         games = YouTubeSearch.objects.all()
-        
+
         for game in games:
             print('------------')
             print(game.id)
             print('------------')
         '''
-        
+
+        return Response(data=ndata)
+
+    def get_video_data(self,id):
+        YouTubeSearch.objects.all().delete()
+        header = {'user-key': 'AIzaSyDmDXP_gaB7cog4f0slbbdJ3RACsY5WQIw',
+        'Accept': 'application/json'}
+        url = 'https://www.googleapis.com/youtube/v3/videos?part=statistics&id={}&key={}'.format('cAB2rWSW4w0', header['user-key'])
+        data = requests.get(url)
+        ndata = data.json()
+
+        filtered_video_data = self.filter_video_data(ndata)
+        self.save_youtube_search(filtered_video_data)
 
         return Response(data=ndata)
 
     def filter_data(self, gamedata):
-        
-        if 'regionCode' in gamedata:
-            regionCode = gamedata['regionCode']
-        else:
-            regionCode = None
-        
-        
-        
+
         #print('aqui em cima: {}'.format(gamedata['items'][0]['id']['videoId']))
-        for i in range(0,50):
+        for i in range(0,49):
             if 'items' in gamedata:
                 if 'id' in gamedata['items'][i]:
                     if 'videoId' in gamedata['items'][i]['id']:
                         id = gamedata['items'][i]['id']['videoId']
-                        print('id aqui: ', id)
+                        print('\nid aqui: ', id)
                     else:
                         id = None
                 else:
                     id = None
             else:
                 id = None
-        
-            '''            
-            if 'name' in gamedata:
-                name = gamedata['name']
-            else:
-                name = None
-            
-            
-           
-        if 'items' in gamedata:    
-            
-            if 'viewCount' in gamedata['items']:
-                count_views=gamedata['items']['viewCount']
-            else:
-                count_views=None
 
-            if 'likeCount' in gamedata['items']:
-                count_likes=gamedata['items']['likeCount']
+            filtered_data = {
+            'videoId': videoId,
+            }
+
+            return filtered_data
+
+
+    def filter_video_data(self, videodata):
+
+        if 'Unid' in videodata:
+
+            if 'viewCount' in videodata['Unid'][0]:
+                count_views=videodata['Unid'][0]['viewCount']
             else:
                 count_views=None
 
-            if 'dislikeCount' in gamedata:
-                count_dislikes=gamedata['items']['dislikeCount']
+            if 'likeCount' in videodata['Unid'][0]:
+                count_likes=videodata['Unid'][0]['likeCount']
+            else:
+                count_views=None
 
-            if 'commentCount' in gamedata:
-                count_comments= gamedata['items']['commentCount']
+            if 'dislikeCount' in videodata['Unid'][0]:
+                count_dislikes=videodata['Unid'][0]['dislikeCount']
+
+            if 'favoriteCount' in videodata['Unid'][0]:
+                count_favorites=videodata['Unid'][0]['favoriteCount']
+
+            if 'commentCount' in videodata['Unid'][0]:
+                count_comments= videodata['Unid'][0]['commentCount']
             else:
                 count_comments = None
-
-            if 'regionCode' in gamedata:
-                regionCode= gamedata['items']['regionCode']
-            else:
-                regionCode = None
 
         else:
             count_views=None
             count_likes=None
             count_dislikes=None
+            count_favorites=None
             count_comments=None
-            '''
-            
-        filtered_data = {
-            'id': id,
-            #'name': name,
-            #'count_views': count_views,
-            #'count_likes': count_likes,
-            #'count_dislikes': count_dislikes,
-            #'count_comments': count_comments,
-            'regionCode': regionCode
-        }
-        return filtered_data
 
-    def save_youtube_search(self, filtered_data):
+            filtered_video_data = {
+            'count_views': count_views,
+            'count_likes': count_likes,
+            'count_dislikes': count_dislikes,
+            'count_favorites': count_favorites,
+            'count_comments': count_comments,
+            }
+
+            return filtered_video_data
+
+
+    def save_youtube_search(self, filtered_data, filtered_video_data):
         results = YouTubeSearch(
             id = filtered_data['id'],
-            #name = filtered_data['name'],
-            #count_views = filtered_data['count_views'],
-            #count_likes = filtered_data['count_likes'],
-            #count_dislikes = filtered_data['count_dislikes'],
-            #count_comments = filtered_data['count_comments'],
-            regionCode = filtered_data['regionCode']
+            count_views = filtered_video_data['count_views'],
+            count_likes = filtered_video_data['count_likes'],
+            count_dislikes = filtered_video_data['count_dislikes'],
+            count_favorites = filtered_video_data['count_favorites'],
+            count_comments = filtered_video_data['count_comments'],
         )
-        
+
         results.save()
-        print('Id video: {}\nRegion Code: {}\n'.format(results.id, results.regionCode))
+        print('Id video: {}\n'.format(results.id))
