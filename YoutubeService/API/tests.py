1b2c4ee4d373b72b15335364fca2ee7a89304682
lmmLucasMachado <lmlucasmachadom@gmail.com>
2018-09-19 22:13:46 -0300

    [ADD] fga-eps-mds/2018.2-GamesBI#134 Put test for the API
    Co-authored-by: Arthut Rodrigues <arthurarp97@gmail.com>

--- YoutubeService/API/tests.py
+++ YoutubeService/API/tests.py
@@ -1,39 +1,42 @@
 from django.test import TestCase
 from model_mommy import mommy
-from Youtubservice.importdata.model import Game
-from rest_framework.test import APITesteCase, URLPatternsTestCase
+from YoutubeService.importdata.models import Game
+from rest_framework.test import APITestCase, URLPatternsTestCase
 from django.urls import include, path, reverse
 from rest_framework import status
 
 # Create your tests here.
-class EndpointsTestCase (APITesteCadse, URLPatternsTestCase):
+class EndpointTestCase(APITestCase, URLPatternsTestCase):
 
     urlpatterns= [
-        path('api/', include('Youtubservice.API.urls'))
+        path('api/', include('YoutubeService.API.urls'))
     ]
 
-    def setUP(self):
-        self.Game = mommy.make(
+    def setUp(self):
+
+        self.Game_yotube = mommy.make(
             Game,
             id = 0,
             name = "teste",
             count_views = 0,
             count_likes = 0,
         )
-        self.youtube_endpoint = reverse('get_igdb_games_id_youtube_list')
-#       self.name_endpoint = reverse('get_youtube_games_Name_list')
+
+        self.youtube_endpoint = reverse('get_youtube_games_Name_list')
+
     def tearDown(self):
+
         Game.objects.all().delete()
 
     def test_status_youtube_endpoint(self):
+
         response = self.client.get(self.youtube_endpoint)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_response_youtube_endpoint(self):
 
         response = self.client.get(self.youtube_endpoint,format = 'json')
-        self.assertEqual(Game.objects.all().count(), 1)
 
-        for jogo in response.data:
-            self.assertNotEqual(response.data['nome'],None)
-        
+        for data in response.data:
+            self.assertNotEqual(data['id'], None)
+            self.assertNotEqual(data['name'], None)
